57281eef9936d506a895a7f0530a4450b83ffc3c
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2010-11-18 14:20:33 +0100

    Created foundation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RingioPluginHighrise::Application.config.secret_token = '5fc01bd063c02c799a1cbca34ab027b550f9bdecbdf272d9dac68d5969fad325345baa4bffc8d65ad65beb7bb9c208ba5690af446ac47aec9850aa533430708c'
