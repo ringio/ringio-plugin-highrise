727ddc0ba4cf47de38b54bf2a48c915fc6e73830
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2010-11-30 17:39:14 +0100

    Fixed a validation for UserMap and added some style for errors and notices.

--- app/models/user_map.rb
+++ app/models/user_map.rb
@@ -2,24 +2,14 @@ class UserMap < ActiveRecord::Base
 
   belongs_to :account
   
-  validates_presence_of :rg_user_id, :hr_user_token, :account_id
-  validates_uniqueness_of :rg_user_id, :hr_user_token
+  validates_presence_of :account_id, :hr_user_id, :rg_user_id, :hr_user_token 
+  validates_uniqueness_of :hr_user_id, :rg_user_id, :hr_user_token
 
-  after_destroy do |um|
-    # if the master user is destroyed, choose a new master user
-    if um.master_user
-      new_master = UserMap.find_by_account_id um.account_id
-      if new_master
-        unless new_master.master_user
-          new_master.master_user = true
-          new_master.save
-        end
-      end
-    end
+  before_validation do |um|
+    um.hr_user_id = hr_user ? hr_user.id : nil
   end
-  
+
   before_save do |um|
-    um.hr_user_id = hr_user.id
     # keep only 1 master user per account
     if um.master_user
       # careful with these methods, no validations or callbacks are used
@@ -35,12 +25,29 @@ class UserMap < ActiveRecord::Base
       end
     end
   end
+
+  after_destroy do |um|
+    # if the master user is destroyed, choose a new master user
+    if um.master_user
+      new_master = UserMap.find_by_account_id um.account_id
+      if new_master
+        unless new_master.master_user
+          new_master.master_user = true
+          new_master.save
+        end
+      end
+    end
+  end
   
   private
     def hr_user
       ApiOperations.set_hr_base self.account.hr_subdomain, self.hr_user_token
 
-      user_hr = Highrise::User.me
+      begin
+        user_hr = Highrise::User.me
+      rescue ActiveResource::UnauthorizedAccess => e
+        self.errors[:hr_user_token] = I18n.t('user_map.unauthorized_token')
+      end
 
       ApiOperations.empty_hr_base
       
