460aff42778d6360c0194b9130b1775e6bb31bfc
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2010-12-22 18:23:07 +0100

    Fixed bug in ring maps.

--- app/models/ring_map.rb
+++ app/models/ring_map.rb
@@ -2,8 +2,8 @@ class RingMap < ActiveRecord::Base
 
   belongs_to :contact_map
   
-  validates_presence_of :contact_map_id, :rg_ring_id, :hr_note_id 
-  validates_uniqueness_of :rg_ring_id, :hr_note_id
+  validates_presence_of :contact_map_id, :rg_ring_id, :hr_ring_note_id 
+  validates_uniqueness_of :rg_ring_id, :hr_ring_note_id
   
   def hr_resource_ring_note
     Highrise::Note.find self.hr_note_id
